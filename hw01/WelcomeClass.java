e96843136d1b0f97bbd276d5d126768495038bb3
kaj219 <kaj219@lehigh.edu>
2015-08-31 23:55:36 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Kang Ji
+//HW01
+//WelcomeClass Java Program
+
+//  first compile the program
+//       javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+    
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("     ^     ^     ^     ^     ^     ^   ");
+    System.out.println("   /   \\ /   \\ /   \\ /   \\ /   \\ /   \\ ");
+    System.out.println("<--- K --- A --- J --- 2 --- 1 --- 9 --->");
+    System.out.println("   \\   / \\   / \\   / \\   / \\   / \\   /   ");
+    System.out.println("     v     v     v     v     v     v");
+    System.out.println("I went back to China this Summer");
+    System.out.println("test");
+    
+  }
+}
